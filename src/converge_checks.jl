8c59a940f78f8680cf2f60327597755f68d1e05d
Karl Pierce <kmp5@vt.edu>
2025-02-24 21:38:22 -0500

    Add verbose to nocheck

--- src/converge_checks.jl
+++ src/converge_checks.jl
@@ -10,7 +10,11 @@ mutable struct NoCheck <: ConvergeAlg
     NoCheck(max) = new(0, max, -1)
 end
 
-function check_converge(check::NoCheck, factors, λ, partial_gram)
+function check_converge(check::NoCheck, factors, λ, partial_gram; verbose = false)
+    rank = ind(λ, 1)
+    if (verbose)
+        println("$(dim(rank))\t $(check.counter)")
+    end
     if check.counter ≥ check.max_counter
         return true
     end
