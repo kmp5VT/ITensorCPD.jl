1bee2a83e32623e666e2281b9f6e824638d39e91
Karl Pierce <kmp5@vt.edu>
2025-02-24 21:38:09 -0500

    Remove depricated algorithm.

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -44,61 +44,12 @@ function mttkrp(::direct, factors, cp, rank::Index, fact::Int)
     return m
 end
 
-struct square_lattice <: MttkrpAlgorithm end
-
-## In this one we have essentially contracted all of the factors in
-## and that is stored in cp.additional_items[:partial_mtkrp]
-## it looks like this for the square lattice
-##   r     r
-##   |  x  | 
-##   1 --- 2 
-##  w|    z| 
-##   4 --- 3 
-##   |  y  | 
-##   r     r
-## Where r is the hyperedge. We can choose to contract all of these except the \c fact
-## To form the environment for each r. Then we just need to contract in the 
-##   b     r              
-##   |  x  |              f ------- r
-## a-1 --- 2            b |  
-##  w|    z|   -->    a - 1 --- E - r
-##   4 --- 3                w,x
-##   |  y  | 
-##   r     r
-## where E is the contracted environement for each r. Then we will contract the bth factor matrix in
-
-function mttkrp(::square_lattice, factors, cp, rank::Index, fact::Int)
-    m = similar(factors[fact])
-    fact_even = iseven(fact)
-    other = fact_even ? fact - 1 : fact + 1
-    effective_fact = fact_even ? fact ÷ 2 : fact ÷ 2 + 1
-
-    ## This forms the second factor in the pair times the 
-    p = factors[other] * cp.target[effective_fact]
-
-    factor_portion = cp.additional_items[:partial_mtkrp][1:end.!=effective_fact]
-    for i = 1:dim(rank)
-        E = contract(map(x -> itensor(array(x)[i, :, :], inds(x)[2:end]), factor_portion))
-        array(m)[i, :] = array(itensor(array(p)[i, :, :, :], inds(p)[2:end]) * E)
-    end
-
-    return m
-end
-
-function post_solve(::square_lattice, factors, λ, cp, rank::Index, fact::Integer)
-    ## Once done with 2 factor, multiply the two into the target and set in the partial_mtkrp list
-    if iseven(fact)
-        effective_fact = fact ÷ 2
-        for i = 1:dim(rank)
-            array(cp.additional_items[:partial_mtkrp][effective_fact])[i, :, :] = array(
-                itensor(array(factors[fact])[i, :], ind(factors[fact], 2)) * (
-                    itensor(array(factors[fact-1])[i, :], ind(factors[fact-1], 2)) *
-                    cp.target[effective_fact]
-                ),
-            )
-        end
-    end
-end
+################
+## This solver is based on ITensorNetwork
+## It allows one to take a completely connected 
+## ITensorNetwork and decomposes it into a CPD
+## You can't currently take a network which is and
+## outer product of two networks
 
 struct network_solver <: MttkrpAlgorithm end
 
@@ -147,3 +98,7 @@ function post_solve(::network_solver, factors, λ, cp, rank::Index, fact::Intege
         cp.additional_items[:partial_mtkrp][partial_ind] = p
     end
 end
+
+## TODO
+## This next code is going to take the CPD
+## of a network that has one CPD rank.
