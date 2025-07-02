d7d59d5e34e222f4a7b7c5fa332c3286f30b18b2
Karl Pierce <kmp5@vt.edu>
2025-07-02 16:37:18 -0400

    Organziation update

--- src/ITensorCPD.jl
+++ src/ITensorCPD.jl
@@ -1,12 +1,12 @@
 module ITensorCPD
 include("row_norm.jl")
 include("algorithms.jl")
-include("pseudoinverse.jl")
-include("had_contract.jl")
+include("algebra/pseudoinverse.jl")
+include("algebra/had_contract.jl")
 include("cpd.jl")
 include("converge_checks.jl")
 include("optimizers/als.jl")
 include("optimizers/decompose.jl")
 include("reconstruct.jl")
-include("cp_contract.jl")
+include("algebra/cp_contract.jl")
 end
