09a1e92a13cfe406fc6d1edbb7f638601d49be8b
MengZhaonan1998 <mengzn1998@gmail.com>
2025-07-21 16:41:45 -0400

    test pivot hadamard product

--- test/pivot_mapping.jl
+++ test/pivot_mapping.jl
@@ -32,6 +32,40 @@ function column_to_multi_coords(col_indices, dims)
     return coords
 end
 
+function pivot_hadamard(B::ITensor, C::ITensor, r::Index, pivot_indices::Array)
+    # We first assume that the first mode of given tensors should match with the given index r
+    if inds(B)[1] != r || inds(C)[1] != r
+        throw("The first dimension of input iTensors does not match with the given r.")
+    end 
+
+    # Map matrix pivot indices to tensor indices
+    pivot_dims = (dim(inds(B)[2]), dim(inds(C)[2]));
+    bc_coordinates = column_to_multi_coords(pivot_indices, pivot_dims);
+    
+    # Hadamard product of B and C in selected pivot indices
+    l = Index(length(pivot_indices), "l")  
+    T = ITensor(r, l)
+    for i in 1:dim(r)
+        for j in 1:dim(l)
+            T[i,j] = B[i, bc_coordinates[j][1]] * C[i, bc_coordinates[j][2]]
+        end
+    end
+
+    return T  
+end
+
+
+k_dim, j_dim, r_dim = 6, 5, 4;
+k = Index(k_dim, "k");
+j = Index(j_dim, "j");
+r = Index(r_dim, "r");
+B = randomITensor(r,j);
+C = randomITensor(r,k);
+
+pivots = [27, 4, 13];
+T = pivot_hadamard(B, C, r, pivots);
+
+
 # TEST 1
 let 
     # Random tensor
