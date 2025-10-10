530adb2cde77666a1a072d693a19a88ddc25d08b
Israa fakih <israa.fakih@epfl.ch>
2025-10-10 17:04:40 +0200

    Add SketchProjected algorithm

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,5 +1,6 @@
 using ITensors: Index
 using ITensors.NDTensors: data
+println("algorithms.jl loaded")
 
 ## These are solvers which take advantage to the canonical CP-ALS normal equations
 abstract type MttkrpAlgorithm end
@@ -383,6 +384,36 @@ end
         end
 
         function post_solve(::PivotBasedSolvers, als, factors, λ, cp, rank::Index, fact::Integer) end
+
+## This solver is for computing Tomega = A(B ododt C)omega 
+## The c1 and C2x vectors are constant vectors for determing the 
+## Sketching dimension and sparsity parameter.
+struct SketchProjected <: ProjectionAlgorithm
+    C1_vect
+    C2_vect
+end
+      SketchProjected()=SketchProjected(nothing,nothing)
+
+    C1_vect(alg::SketchProjected) = alg.C1_vect
+    C2_vect(alg::SketchProjected) = alg.C2_vect
+
+
+    function project_krp(::SketchProjected, als, factors, cp, rank::Index, fact::Int)
+        return ITensorCPD.omega_hadamard(factors, rank, als.additional_items[:sketch_matrices][fact])
+    end
+
+    function matricize_tensor(:: SketchProjected, als, factors, cp, rank::Index, fact::Int)
+        return als.additional_items[:target_transform][fact]
+    end
+
+    function solve_ls_problem(::SketchProjected, projected_KRP, project_target, rank)
+        direction = qr(array(projected_KRP), ColumnNorm()) \ transpose(array(project_target))
+        i = ind(project_target,1)
+        return itensor(copy(transpose(direction)), i ,rank)
+    end
+
+    function post_solve(::SketchProjected, als, factors, λ, cp, rank::Index, fact::Integer) end
+
 
 
     ## This solver does not form the normal equations. 
--- src/optimizers/ALS/als.jl
+++ src/optimizers/ALS/als.jl
@@ -168,6 +168,7 @@ function compute_als(
         Ris = uniqueinds(target, i)
         Tmat = reshape(array(target, (i, Ris...)), (dim(i), dim(Ris)))
         _, _, p = qr(Tmat, ColumnNorm())
+    
         #_,_,p = lu(Tmat', RowMaximum(), allowsingular=true)
         push!(pivots, p)
 
@@ -228,7 +229,7 @@ function compute_als(
             m = dim(i)
             l=Int(round(3 * m * log(m))) 
             s=Int(round(log(m)))
-             _,_,p = SEQRCS(target,n,i,l,s,k_sk)
+            _,_,p = SEQRCS(target,n,i,l,s,k_sk)
         else
             Tmat = reshape(array(target, (i, Ris...)), (dim(i), dim(Ris)))
             _, _, p = qr(Tmat, ColumnNorm())
@@ -245,6 +246,37 @@ function compute_als(
     extra_args[:projects] = pivots
     extra_args[:projects_tensors] = projectors
     extra_args[:target_transform] = targets
+    
+    return ALS(target, alg, extra_args, check)
+end
+
+function compute_als(
+    alg::SketchProjected,
+    target::ITensor,
+    cp::CPD{<:ITensor};
+    extra_args = Dict(),
+    check = nothing,
+)
+    C1_v = C1_vect(alg)
+    C2_v = C2_vect(alg)
+    targets = Vector{ITensor}()
+    sketch = Vector{Matrix{Float64}}()
+    for (i, n) in zip(inds(target), 1:length(cp))
+        C1 = isnothing(C1_v) ? 3 : C1_v[n]
+        C2 = isnothing(C2_v) ? 1 : C2_v[n]
+        Ris = uniqueinds(target, i)         
+        p = dim(Ris)
+        m = dim(i)
+        l=Int(round(C1 * m * log(m))) 
+        s=Int(round(C2*log(m)))
+        omega = sparse_sign_matrix(l,p,s)
+        TP = sketched_matricization(target, n, omega')
+        TP = itensor(TP,i,Index(l,"l"))
+    push!(targets, TP)
+    push!(sketch,omega)
+    end
+    extra_args[:target_transform] = targets
+    extra_args[:sketch_matrices] = sketch
     
     return ALS(target, alg, extra_args, check)
 end
