cd481da7350aa4d855b33e14bafa735053429a15
Karl Pierce <karl.m.pierce@gmail.com>
2025-11-02 16:09:53 -0500

    Remove timers

--- src/algebra/SEQRCS.jl
+++ src/algebra/SEQRCS.jl
@@ -43,8 +43,6 @@ function sparse_sign_matrix(l::Int, n::Int, s::Int)
     return sparse(rows .+ 1, cols .+ 1, vals, l, n)
 end
 
-using TimerOutputs
-
 """
 SEQRCS(A,l,s,k,t)
 
@@ -61,52 +59,43 @@ Performs Randomized QR to a matrix 'A' for a given rank 'k'
 
 """
 function SEQRCS(A:: ITensor,mode::Int,i,l,s,t)
-    timer = TimerOutput()
 
     Ris = uniqueinds(A, i)         
     n = dim(Ris)
 
     # Generate sparse embedding
-    @timeit timer "Omega" omega = sparse_sign_matrix(l,n,s)
+    omega = sparse_sign_matrix(l,n,s)
 
     # Sketch the matrix and applying QR 
-    @timeit timer "sketching" A_sk = sketched_matricization(A, mode , omega')
+    A_sk = sketched_matricization(A, mode , omega')
     
-    @timeit timer "QR1" begin 
     _, _, p_sk = qr(A_sk, ColumnNorm())  
     p_sk=p_sk[1:t]
     println("The size of A_sk is $(size(A_sk))")
-    end
 
     ## Map back  pivots from 'A_sk' to 'A' and forming 'A_subset'
-    @timeit timer "row select" begin
     rows_sel = omega[p_sk,:]
-    @timeit timer "findall" indices = findall(col -> any(!=(0), col), eachcol(rows_sel))
+    indices = findall(col -> any(!=(0), col), eachcol(rows_sel))
     indices_ind = Index(length(indices),"ind")
     indices_tensor = itensor(Int, indices, indices_ind)
-    @timeit timer "fused_flatten" A_subset = fused_flatten_sample(A, mode, indices_tensor)
+    A_subset = fused_flatten_sample(A, mode, indices_tensor)
     A_subset = array(A_subset)
     println("The size of A_subset is $(length(indices))")
-    end
 
     ## Perform QR on A_subset to get final 'k' pivots
-    @timeit timer "qr2" begin
     Q, R, p_subset = qr(A_subset, ColumnNorm()) 
     rem_indices = setdiff(1:n,indices)
     p = vcat(indices[p_subset],rem_indices)
-    end
 
     ## Form  A_rem to get the factor 'R' 
     ## We can remove this part no need to get Q and R
     ## but keeping it just to make sure that the function is performing well
-    @timeit timer "Reconstruct" begin
     rem_indices_ind = Index(length(rem_indices),"rem_ind")
     rem_indices_tensor = itensor(rem_indices, rem_indices_ind)
     A_rem = fused_flatten_sample(A, mode, rem_indices_tensor)
     A_rem = matrix(A_rem)
     R = hcat(R,Q'*A_rem)
-    end
-    # @show timer
+
     return Q,R,p
 
 end
