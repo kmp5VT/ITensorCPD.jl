e96a8822ac053cb8ff710cac478054bd54e72859
kmp5VT <kmp5@vt.edu>
2025-11-17 16:08:21 -0500

    Move abstract GPU arrays defition inside of ITensorCPD

--- src/algebra/ldiv_solve.jl
+++ src/algebra/ldiv_solve.jl
@@ -1,7 +1,12 @@
 using ITensors.NDTensors.Expose
+using GPUArraysCore: AbstractGPUArray
 
 function ldiv_solve!!(A::Exposed, B::Exposed; factorizeA = false)
     return ldiv_solve!(unexpose(A), unexpose(B); factorizeA)
+end
+
+function ldiv_solve!!(A::Exposed{<:AbstractGPUArray}, B::Exposed{<:AbstractGPUArray}; factorizeA = false)
+    return ldiv_solve!(A,B; factorizeA = false)
 end
 
 ## For now just call with QR CP if factorize. Later this will be more complex.
