e33b3db62c0583b5558ace1516da1aa8e94d8d40
Karl Pierce <karl.m.pierce@gmail.com>
2026-07-01 15:49:23 -0400

    Update ldiv for high dimensional problems by scaling

--- src/algebra/ldiv_solve.jl
+++ src/algebra/ldiv_solve.jl
@@ -11,19 +11,36 @@ end
 
 ## For now just call with QR CP if factorize. Later this will be more complex.
 function ldiv_solve!(A, B; factorizeA = false)
+    # MeanDag = mean(diag(A))
     if factorizeA 
         szA = size(A)
         if (szA[1] == szA[2])
+            evals = eigvals(A)
+            MeanDag = sum(evals)
+            (A) .*= 1/(MeanDag)
+            Bscale = B .* 1/(MeanDag)
             try
-                return cholesky(Hermitian(A), RowMaximum(), check=true, tol=cholesky_epsilon) \ B
+                return cholesky(Hermitian(A), RowMaximum(), check=true, tol=cholesky_epsilon) \ Bscale
             catch
                 # println("Warning: Cholesky based solver failed.")
-                return qr(A, ColumnNorm()) \ B
+                evals = svdvals(A)
+                MeanDag = sum(evals)
+                (A) .*= 1/(MeanDag)
+                Bscale = B .* 1/(MeanDag)
+                return qr(A, ColumnNorm()) \ Bscale
             end
         else
-            return qr(A, ColumnNorm()) \ B
+            evals = svdvals(A)
+            MeanDag = sum(evals)
+            (A) .*= 1/(MeanDag)
+            Bscale = B .* 1/(MeanDag)
+            return qr(A, ColumnNorm()) \ Bscale
         end
     else
-        return A \ B
+        evals = svdvals(A)
+        MeanDag = sum(evals)
+        (A) .*= 1/(MeanDag)
+        Bscale = B .* 1/(MeanDag)
+        return A \ Bscale
     end
 end
